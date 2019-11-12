6850332d6f787b482e2563e984ce5a372c65d93e
nagutao <guilherme_tonello@hotmail.com>
2019-11-11 22:41:50 -0300

    appointment check maded

--- src/config/database.js
+++ src/config/database.js
@@ -1,9 +1,9 @@
 module.exports = {
   dialect: 'postgres',
-  host: '192.168.99.100',
+  host: 'localhost',
   username: 'postgres',
   password: 'docker',
-  database: 'goBarber',
+  database: 'gobarber',
   define: {
     timestamps: true,
     underscored: true,
